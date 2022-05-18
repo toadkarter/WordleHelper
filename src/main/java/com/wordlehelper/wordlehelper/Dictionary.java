f13a000aef8e0bddb2b744b389b21f653db32067
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-05-18 17:56:19 +0100

    Trying to read word frequencies into hashmap

--- src/main/java/com/wordlehelper/wordlehelper/Dictionary.java
+++ src/main/java/com/wordlehelper/wordlehelper/Dictionary.java
@@ -2,6 +2,7 @@ package com.wordlehelper.wordlehelper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -9,6 +10,7 @@ import java.util.Set;
 // Credit: https://github.com/dwyl/english-words
 public class Dictionary {
     private final Set<String> dictionary = new HashSet<>();
+    private final HashMap<String, Double> dictionaryFrequencies = new HashMap<>();
 
     public Dictionary() throws FileNotFoundException {
         Scanner textFile = new Scanner(new File("src/words.txt"));
@@ -16,6 +18,17 @@ public class Dictionary {
             dictionary.add(textFile.next().trim().toLowerCase());
         }
         textFile.close();
+
+        Scanner frequencies = new Scanner(new File("src/word_frequencies.txt"));
+        while (frequencies.hasNext()) {
+            String[] currentLine = frequencies.next().split(":", 2);
+            String word = currentLine[0];
+            Double frequency = Double.parseDouble(currentLine[1]);
+            dictionaryFrequencies.put(word, frequency);
+        }
+
+        System.out.println(dictionaryFrequencies);
+
     }
 
     public boolean isInDictionary(String word) {
