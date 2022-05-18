7cfd4bbd31c7ca36096f635fc9e7a84abe862905
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-05-18 17:58:32 +0100

    Successfully converted new wordlist to hashmap

--- src/main/java/com/wordlehelper/wordlehelper/Dictionary.java
+++ src/main/java/com/wordlehelper/wordlehelper/Dictionary.java
@@ -9,30 +9,21 @@ import java.util.Set;
 
 // Credit: https://github.com/dwyl/english-words
 public class Dictionary {
-    private final Set<String> dictionary = new HashSet<>();
-    private final HashMap<String, Double> dictionaryFrequencies = new HashMap<>();
+    private final HashMap<String, Double> dictionary = new HashMap<>();
 
     public Dictionary() throws FileNotFoundException {
-        Scanner textFile = new Scanner(new File("src/words.txt"));
+        Scanner textFile = new Scanner(new File("src/word_frequencies.txt"));
         while (textFile.hasNext()) {
-            dictionary.add(textFile.next().trim().toLowerCase());
-        }
-        textFile.close();
-
-        Scanner frequencies = new Scanner(new File("src/word_frequencies.txt"));
-        while (frequencies.hasNext()) {
-            String[] currentLine = frequencies.next().split(":", 2);
+            String[] currentLine = textFile.next().split(":", 2);
             String word = currentLine[0];
             Double frequency = Double.parseDouble(currentLine[1]);
-            dictionaryFrequencies.put(word, frequency);
+            dictionary.put(word, frequency);
         }
-
-        System.out.println(dictionaryFrequencies);
-
+        textFile.close();
     }
 
     public boolean isInDictionary(String word) {
-        return dictionary.contains(word);
+        return dictionary.containsKey(word);
     }
 
 }
