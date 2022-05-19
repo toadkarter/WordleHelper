fc9675db7dc91a436892986ab14342767fab7538
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-05-19 09:56:11 +0100

    Removed Maven and added standard module dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,6 @@ module com.wordlehelper.wordlehelper {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-
     opens com.wordlehelper.wordlehelper to javafx.fxml;
     exports com.wordlehelper.wordlehelper;
     exports com.wordlehelper.wordlehelper.model;
